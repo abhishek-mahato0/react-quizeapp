e151a2e92dca3ac92c19e4a1fef13302d07cdbae
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2022-05-02 18:23:58 +0545

    Deploying quize app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import data from './data';
+import money from './money';
 
 function App() {
+  const[classlist,setClasslist]=useState("option");
+  const [ques,setQues] = useState(0);
+  const [ans,setAns] = useState(null);
+  const[score,setScore]=useState(0);
+  const[container,setContainer]= useState("container");
+  const [prize,setPrize] = useState(0);
+  const [earned,setEarned]= useState("none");
+  const [moneyclass,setMoneyclass]= useState("prize");
+  const [btns,setbtns]=useState("none");
+  const next=()=>{
+    setClasslist("option");
+    setContainer("container");
+    setMoneyclass("prize");
+    setEarned("none");
+    setbtns("none");
+    setQues(0);
+  }
+  const checkanswers=(ele)=>{
+    setAns(ele);
+    setClasslist("option selected");
+    setTimeout(() => {
+      if(ele.correct===true){
+        setClasslist("option correct");
+        setScore(score+1);
+        setTimeout(() => {
+          if(data.length-1===ques){
+            setContainer("none");
+            setEarned("flex")
+            setMoneyclass("none");
+            setbtns("flex")
+          }
+          else{
+            setQues(ques+1);
+          }
+        }, 1000);
+      }
+      else{
+        setClasslist("option wrong");
+        setTimeout(()=>{
+          setContainer("none");
+          setEarned("flex");
+          setMoneyclass("none");
+          setbtns("flex");
+        },800)
+      }
+    },1000);
+   
+  }
+  useEffect(()=>{
+    setPrize(data[ques].id>1 ? money.find((m)=>m.id===data[ques].id-1).mon:0);
+  },[ques])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="earned">
+      <div className={earned}>
+        <h1>You Earned:{prize}</h1>
+      </div>
+      <div className={btns}>
+          <div className="next"><button onClick={next}>Play Again</button></div>
+        </div>
+      </div>
+     <div className="smaller">
+       <h1>Your Earning is Rs.{prize}</h1>
+     </div>
+      <div className={container}>
+        <div className="ques">
+          <h1>{data[ques].id}. {data[ques].ques}</h1>
+        </div>
+        <div className="options">
+          {data[ques].options.map((ele)=>{
+            return(
+              <div className={ ans===ele ? classlist : "option"} onClick={()=>checkanswers(ele)}>
+              {ele.option}
+            </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className={moneyclass}>
+          {
+            money.map((elem)=>{
+              return(
+                  <h3 className={elem.id===data[ques].id ? "active" : "money"} >{elem.mon}</h3>
+              )
+            })
+          }
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,229 @@
+
+
+const data = [
+    {
+        id:1,
+        ques:"Which river flows through the Scottish city of Glasgow?",
+        options:[
+            {
+                option:"Tay",
+                correct:false
+            },
+            {
+                option:"Dee",
+                correct:false
+            },
+            {
+                option:"Tweed",
+                correct:false
+            },
+            {
+                option:"Clyde",
+                correct:true
+            }
+        ],
+    },
+    {
+        id:2,
+        ques:"Rolex is a company that specializes in what type of product?",
+        options:[
+            {
+                option:"Watch",
+                correct:true
+            },
+            {
+                option:"Car",
+                correct:false
+            },
+            {
+                option:"Cloth",
+                correct:false
+            },
+            {
+                option:"Shoe",
+                correct:false
+            }
+        ],
+   
+    },
+    {
+        id:3,
+        ques:"What temperature does water boil at?",
+        options:[
+            {
+                option:"50 degree C",
+                correct:false
+            },
+            {
+                option:"100 degree Clecius",
+                correct:true
+            },
+            {
+                option:"150 degree Celcius",
+                correct:false
+            },
+            {
+                option:"90 degree Celcius",
+                correct:false
+            }
+        ],
+   
+    },
+    {
+        id:4,
+        ques:"Which is the only mammal that can’t jump?",
+        options:[
+            {
+                option:"Giraffe",
+                correct:false
+            },
+            {
+                option:"Human",
+                correct:false
+            },
+            {
+                option:"Elephant",
+                correct:true
+            },
+            {
+                option:"Lion",
+                correct:false
+            }
+        ],
+    },
+    {
+        id:5,
+        ques:"Where is the smallest bone in the human body?",
+        options:[
+            {
+                option:"The Ear",
+                correct:true
+            },
+            {
+                option:"The Chest",
+                correct:false
+            },
+            {
+                option:"The Thigh",
+                correct:false
+            },
+            {
+                option:"Hand",
+                correct:false
+            }
+        ],
+   
+    },
+    {
+        id:6,
+        ques: "Who painted the Mona Lisa?",
+        options:[
+            {
+                option:"Alexander Graham Bell",
+                correct:false
+            },
+            {
+                option:"Sir Isaac Newton",
+                correct:false
+            },
+            {
+                option:"Leonardo Da Vinci",
+                correct:true
+            },
+            {
+                option:"Albert Einstein",
+                correct:false
+            }
+        ]
+    },
+    {
+        id:7,
+        ques:"What’s the capital of Ethiopia?",
+        options:[
+            {
+                option:"Cape Town",
+                correct:false
+            },
+            {
+                option:"San Francisco",
+                correct:false
+            },
+            {
+                option:"Addis Ababa",
+                correct:true
+            },
+            {
+                option:"Syndey",
+                correct:false
+            },
+        ]
+    },
+    {
+        id:8,
+        ques:"What are the first three words of the bible?",
+        options:[
+            {
+                option:"be with everyone",
+                correct:false
+            },
+            {
+                option:"Again Jesus asked",
+                correct:false
+            },
+            {
+                option:"In the beginning",
+                correct:true
+            },
+            {
+                option: "At that time",
+                correct:false
+            },
+        ]
+    },
+    {
+        id:9,
+        ques:"Who invented the electric light bulb?",
+        options:[
+            {
+                option:"Thomas Edison",
+                correct:true
+            },
+            {
+                option:"Wole Soyinka",
+                correct:false
+            },
+            {
+                option:"Tom Cruise",
+                correct:false
+            },
+            {
+                option:"Barack Obama",
+                correct:false
+            },
+        ]
+    },
+    {
+        id:10,
+        ques:"How many squares are there on a chess board?",
+        options:[
+            {
+                option:120,
+                correct:false
+            },
+            {
+                option:64,
+                correct:true
+            },
+            {
+                option:86,
+                correct:false
+            },
+            {
+                option:76,
+                correct:false
+            },
+        ]
+    }
+]
+
+export default data;
